de431a34d1b2d77b20603bd6721e4181e4e1e740
biel99lol <gabygomes0608@gmail.com>
2026-07-30 20:48:12 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local playersService = clonerf(game:GetService("Players"))
 local player = playersService.LocalPlayer
-local oi = nil
+local oi = false
 
 if (player.Character and player.Character:FindFirstChild("HumanoidRootPart")) and (player.DisplayName == "Biel_TKS" or player.Name == "ROBLOX6666JK") then
     print("é o biel")
